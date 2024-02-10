6ff31274ee217a375f79405ee7e0ce0073907df7
akfrdma0125 <nogain1215@gmail.com>
2024-02-10 22:30:27 +0900

    feat: 카카오맵 기본 설정

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="preload"
+          as="font"
+          type="font/otf"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
