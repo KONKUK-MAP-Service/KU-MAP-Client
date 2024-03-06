cf550c93dea2471d9140f9b4bcbf329562b08613
akfrdma0125 <nogain1215@gmail.com>
2024-03-07 00:46:20 +0900

    feat: 프로필 및 내 마커 관리 반응형 처리

--- components/main/UserMarker.tsx
+++ components/main/UserMarker.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image';
 
 export default function UserMarker() {
   return (
-    <div className="absolute right-0 top-0 w-[180px] h-[48px] bg-white border-2 border-gray-200 rounded-[50px] z-10 flex flex-row items-center justify-center">
+    <div className="my-marker min-h-[50px] min-w-[150px] flex flex-row">
       <div className="w-[10px]" />
       <Image src="/images/marker2.png" alt="userMarker" width={34} height={34} />
-      <div className="w-[15px]" />
-      <div className="w-[120px] h-[19px] text-[18px] text-[#5e5e5e]">내 마커 관리</div>
+      <div className="m-1 text-[#5e5e5e]">내 마커 관리</div>
+      <div className="w-[10px]" />
     </div>
   );
 }
--- components/main/UserProfile.tsx
+++ components/main/UserProfile.tsx
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from 'next/image';
 
-type HeaderProps = {
-  user: String;
-  profileImage: string;
-};
+export default function Header() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [userName, setUserName] = useState("로그인");
+  const [profileImageUrl, setProfileImageUrl] = useState("/images/profile.png");
 
-export default function Header({user, profileImage}: HeaderProps) {
-  const userName = user;
-  let profileImageUrl = profileImage;
-  if (profileImageUrl == null) {
-    profileImageUrl = "/images/profile.png";
-  }
+  useEffect(() => {
+    const accessToken = sessionStorage.getItem("accessToken");
+    if (accessToken) {
+      setUserName("홍길동"); 
+      setProfileImageUrl("/images/anton.jpeg");
+      setIsLogin(true);
+    }
+  }, []);
 
   return (
-    <header className="absolute left-0 top-0 w-[204px] h-[57px] bg-white border-2 border-gray-200 rounded-bl-[50px] rounded-br-[50px] z-10 flex items-center justify-start">
-      <div className="ml-[30px] rounded-full overflow-hidden w-[40px] h-[40px] border-2 border-gray-400">
+    <header className="profile-header min-h-[50px] min-w-[170px] flex flex-row">
+      <div className="rounded-full overflow-hidden w-[40px] h-[40px] border-2 border-gray-400">
         <Image 
           src={profileImageUrl}
-          alt="logo"
-          width={100}
-          height={100}
-          layout="responsive"
+          alt="profile"
+          width={40}
+          height={40}
+
         />
       </div>
-      <span className="ml-[20px]">{userName}</span>
-      <span className="ml-[10px]">님</span>
+      <span className="m-2">{userName}</span>
+      <span>님</span>
     </header>
   );
 }
