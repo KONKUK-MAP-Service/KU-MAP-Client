62c5a944bd95560a0dcc911a803d6dbe8643d11e
akfrdma0125 <nogain1215@gmail.com>
2024-03-24 17:41:08 +0900

    feat: 유저 프로필 플로팅 버튼 수정

--- components/main/UserProfile.tsx
+++ components/main/UserProfile.tsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from "react";
 import Image from 'next/image';
-import Link from "next/link";
 
-export default function Header() {
-  const [isLogin, setIsLogin] = useState(false);
+interface UserProfileProps {
+  onUserProfileClick: () => void;
+}
+
+const UserProfile: React.FC<UserProfileProps> = ({ onUserProfileClick }) => {
   const [userName, setUserName] = useState("로그인");
   const [profileImageUrl, setProfileImageUrl] = useState("/images/profile.png");
 
   useEffect(() => {
-    const accessToken = sessionStorage.getItem("accessToken");
-    if (accessToken) {
-      setUserName("홍길동"); 
-      setProfileImageUrl("/images/anton.jpeg");
-      setIsLogin(true);
+    const loginUser = sessionStorage.getItem("login_user");
+    if (loginUser) {
+      const user = JSON.parse(loginUser);
+      setUserName(user.nickname);
+      setProfileImageUrl(user.profileImage);
     }
   }, []);
 
   return (
     <header className="profile-header min-h-[50px] min-w-[170px] flex flex-row">
-      <Link href="/mypage" className="flex flex-row">
+      <button onClick={onUserProfileClick} className="flex flex-row">
         <div className="rounded-full overflow-hidden w-[40px] h-[40px] border-2 border-gray-400">
           <Image 
             src={profileImageUrl}
@@ -29,7 +31,9 @@ export default function Header() {
           />
         </div>
         <span className="m-2">{userName} 님</span>
-      </Link>  
+      </button>  
     </header>
   );
 }
+
+export default UserProfile;
